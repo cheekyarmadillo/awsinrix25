85e306a142f90193f64b569e94ef3ee872f5b8b0
Ryan Deivert <radeivert@gmail.com>
2025-10-25 16:28:23 -0700

    added debug mode

--- test.py
+++ test.py
@@ -1,6 +1,12 @@
 import kagglehub
 
-path: str = kagglehub.dataset_download("farzadnekouei/pothole-image-segmentation-dataset")
+DEBUG = True
 
-print("Path to dataset files:", path)
+kaggle_data_path: str = kagglehub.dataset_download("farzadnekouei/pothole-image-segmentation-dataset")
+kaggle_images_path: str = f"{kaggle_data_path}/Pothole_Segmentation_YOLOv8/train/images/*"
+
+if DEBUG:
+    import glob
+    print("Path to dataset files:", kaggle_data_path)
+    print(glob.glob(f"{kaggle_images_path}"))
 
