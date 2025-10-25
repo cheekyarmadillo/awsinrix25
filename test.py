7e061d1d213da5b1c56897dbc2e47f70cafed83f
Ryan Deivert <radeivert@gmail.com>
2025-10-25 16:33:47 -0700

    c2v images

--- test.py
+++ test.py
@@ -1,4 +1,6 @@
 import kagglehub
+import cv2
+import glob
 
 DEBUG = True
 
@@ -6,7 +8,14 @@ kaggle_data_path: str = kagglehub.dataset_download("farzadnekouei/pothole-image-
 kaggle_images_path: str = f"{kaggle_data_path}/Pothole_Segmentation_YOLOv8/train/images/*"
 
 if DEBUG:
-    import glob
     print("Path to dataset files:", kaggle_data_path)
-    print(glob.glob(f"{kaggle_images_path}"))
+    print(glob.glob("kaggle_images_path"))
+
+images = [cv2.imread(image) for image in glob.glob(kaggle_images_path)]
+
+for image in images:
+    cv2.imshow("Image", image)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
 
