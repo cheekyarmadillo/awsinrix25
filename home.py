34f3d7f85fdcfd80e0db7382fe0fb8e9cfa78f8b
Ryan Deivert <radeivert@gmail.com>
2025-10-26 03:42:28 -0700

    optimized, image system

--- home.py
+++ home.py
@@ -1,10 +1,13 @@
 from flask import Flask, render_template_string, request, jsonify
+import os
 
 def load_html(path: str) -> str:
     with open(path) as file:
         return file.read()
 
 HOME_HTML = load_html("home.html")
+REPORT_HTML = load_html("report.html")
+MAP_HTML = load_html("map.html")
 
 app = Flask(__name__, static_folder="static") #starts the app
 
@@ -12,6 +15,7 @@ app = Flask(__name__, static_folder="static") #starts the app
 def get_js_data():
     data = request.get_json()
     print(data.get("imageName"));
+    print(os.path.exists(data.get("imageName")))
     
     return jsonify(result=data)
 
@@ -21,14 +25,11 @@ def home():
 
 @app.route("/report")
 def report():
-    with open("report.html", "r", encoding="utf-8") as file:
-        return render_template_string(file.read())
+    return render_template_string(REPORT_HTML)
 
 @app.route("/map")
 def map_page():
-    with open("map.html", "r", encoding="utf-8") as f:
-        return render_template_string(f.read())
-
+    return render_template_string(MAP_HTML)
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=5000, debug=False, use_reloader=False)
